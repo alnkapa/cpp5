6211c1e42010d9d466b1733c09a11ca7f3185400
Alexander A. Kapralov <alnkapa@gmail.com>
2024-05-06 19:40:25 +0300

    ready

--- main.cpp
+++ main.cpp
@@ -4,28 +4,35 @@
 
 int main() {
   auto sst{read(std::cin)};
+  bool enter = false;
   // выводим полный список адресов после сортировки. Одна строка - один
   // адрес.
   for (auto &num : sst) {
-    std::cout << num << "\n";
+    if (!enter) {
+      // первая строка
+      enter = true;
+    } else {
+      std::cout << "\n";
+    }
+    std::cout << num;
   }
   // выводим список адресов, первый байт которых равен 1.
   for (auto &num : sst) {
     if (num.m_num0 == 1) {
-      std::cout << num << "\n";
+      std::cout << "\n" << num;
     }
   }
   // выводим список адресов, первый байт которых равен 46, а второй 70.
   for (auto &num : sst) {
     if (num.m_num0 == 46 && num.m_num1 == 70) {
-      std::cout << num << "\n";
+      std::cout << "\n" << num;
     }
   }
   // выводим список адресов, любой байт которых равен 46.
   for (auto &num : sst) {
     if (num.m_num0 == 46 || num.m_num1 == 46 && num.m_num2 == 46 ||
         num.m_num3 == 46) {
-      std::cout << num << "\n";
+      std::cout << "\n" << num;
     }
   }
   return 0;
--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@
 # mkdir -p build
 #cd build
 # rm -rf *
-cmake .. -DPATCH_VERSION=3  -DBUILD_GMOCK=OFF -DINSTALL_GTEST=OFF -DCMAKE_INSTALL_PREFIX=~
+cmake .. -DCMAKE_INSTALL_PREFIX=~
 cmake --build .
-cmake --build . --target=install
 cmake --build . --target=test
-cmake --build . --target=package
+cat ../ip_filter.tsv | ./ip_filter  | cat > ../rez.txt |  cat  ../rez.txt | md5sum | cat > ../hash.md5
