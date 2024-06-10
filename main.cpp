7df44907ab18d31c0219965f92163f679978ffec
Alexander A. Kapralov <alnkapa@gmail.com>
2024-06-10 15:14:06 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -85,7 +85,7 @@ namespace impl {
         /**
          * для каждого индекса
          */
-        template <typename T, size_t... N>
+        template <typename T, std::size_t... N>
         constexpr void print_tuple(const T &in, std::index_sequence<N...>) {
             (..., (std::cout << (N == 0 ? "" : ".") << std::get<N>(in)));
         }
@@ -105,122 +105,77 @@ namespace impl {
         constexpr void print_ip(const T &in) {
             std::cout << "undefined" << std::endl;
         }
-    }  // namespace iteration3
+    }  // namespace iteration4
     namespace iteration3 {
         /**
-         * проверка на оператор <<
+         * вывод любых контейнеров
          */
-        namespace operator_shift {
-            template <typename T>
-            static auto check(int) -> decltype(std::declval<std::ostream &>() << std::declval<T>(), std::true_type());
-            template <typename>
-            static std::false_type check(...);
-            template <typename U>
-            struct has {
-                static constexpr bool value = decltype(check<U>(int{}))::value;
-            };
-        }  // namespace operator_shift
-        /**
-         * проверка на begin() и end()
-         */
-        namespace begin_end {
-            template <typename T>
-            static auto check(int) -> decltype(std::declval<T>().begin(), std::declval<T>().end(), std::true_type());
-            template <typename>
-            static std::false_type check(...);
-            template <typename U>
-            struct has {
-                static constexpr bool value = decltype(check<U>(int{}))::value;
-            };
-        }  // namespace begin_end
-        namespace iteration3 {
-            template <typename T>
-            struct extract {
-                using type = decltype(std::declval<T>().begin());
-            };
-            /**
-             * вывод любых контейнеров
-             */
-            template <typename T, typename = std::enable_if_t<begin_end::has<T>::value, bool>>
-            constexpr void print_ip(const T &in, int) {
-                bool is_point = false;
-                for (auto it = in.begin(); it != in.end(); it++) {
-                    if (is_point) {
-                        std::cout << ".";
-                    } else {
-                        is_point = true;
-                    }
-                    std::cout << (*it);
+        template <typename T, typename = std::enable_if_t<begin_end::has<T>::value, bool>>
+        constexpr void print_ip(const T &in, int) {
+            bool is_point = false;
+            for (auto it = in.begin(); it != in.end(); it++) {
+                if (is_point) {
+                    std::cout << ".";
+                } else {
+                    is_point = true;
                 }
-                std::cout << std::endl;
+                std::cout << (*it);
             }
-            template <typename T>
-            constexpr void print_ip(const T &in, long) {
-                std::cout << "undefined" << std::endl;
-            }
-            std::cout << (*it);
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
-    }
-    /**
-     * вывод остальных
-     */
-    template <typename T>
-    constexpr void print_ip(const T &in, long) {
-        iteration4::print_ip(in);
-    }
-}  // namespace iteration3
-namespace iteration2 {
-    /**
-     * вывод строк
-     */
-    template <typename T, typename = std::enable_if_t<c_str::has<T>::value, bool>>
-    constexpr void print_ip(const T &in, int) {
-        std::cout << in << std::endl;
-    }
-    /**
-     * вывод остальных
-     */
-    template <typename T>
-    constexpr void print_ip(const T &in, long) {
-        iteration3::print_ip(in, int{});
-    }
-}  // namespace iteration2
-namespace iteration1 {
-    /**
-     * вывод на целочисленных значений
-     */
-    template <typename T, typename = std::enable_if_t<std::is_integral_v<T>, bool>>
-    constexpr void print_ip(const T &in, int) {
-        union {
-            T in;
-            std::uint8_t arr[sizeof(T)];
-        } un;
-        un.in = in;
-        bool is_point = false;
-        for (int i = sizeof(T) - 1; i > -1; i--) {
-            if (is_point) {
-                std::cout << ".";
-            }
-            /**
-             * вывод остальных
-             */
-            template <typename T>
-            constexpr void print_ip(const T &in, long) {
-                iteration3::print_ip(in, int{});
-            }
-            std::cout << int(un.arr[i]);
+        /**
+         * вывод остальных
+         */
+        template <typename T>
+        constexpr void print_ip(const T &in, long) {
+            iteration4::print_ip(in);
         }
-        std::cout << std::endl;
-    }
-    /**
-     * вывод остальных
-     */
-    template <typename T>
-    constexpr void print_ip(const T &in, long) {
-        iteration2::print_ip(in, int{});
-    }
-}  // namespace iteration1
+    }  // namespace iteration3
+    namespace iteration2 {
+        /**
+         * вывод строк
+         */
+        template <typename T, typename = std::enable_if_t<c_str::has<T>::value, bool>>
+        constexpr void print_ip(const T &in, int) {
+            std::cout << in << std::endl;
+        }
+        /**
+         * вывод остальных
+         */
+        template <typename T>
+        constexpr void print_ip(const T &in, long) {
+            iteration3::print_ip(in, int{});
+        }
+    }  // namespace iteration2
+    namespace iteration1 {
+        /**
+         * вывод на целочисленных значений
+         */
+        template <typename T, typename = std::enable_if_t<std::is_integral_v<T>, bool>>
+        constexpr void print_ip(const T &in, int) {
+            union {
+                T in;
+                std::uint8_t arr[sizeof(T)];
+            } un;
+            un.in = in;
+            bool is_point = false;
+            for (int i = sizeof(T) - 1; i > -1; i--) {
+                if (is_point) {
+                    std::cout << ".";
+                }
+                is_point = true;
+                std::cout << int(un.arr[i]);
+            }
+            std::cout << std::endl;
+        }
+        /**
+         * вывод остальных
+         */
+        template <typename T>
+        constexpr void print_ip(const T &in, long) {
+            iteration2::print_ip(in, int{});
+        }
+    }  // namespace iteration1
 }  // namespace impl
 
 /*! функция печати условного IP-адреса
